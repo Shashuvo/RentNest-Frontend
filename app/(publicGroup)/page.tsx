b6c30aabafbab8340d27ff4bd27c3fc9663f60ca
Shashuvo <shahariat2810@gmail.com>
2026-08-15 17:02:10 +0600

    how rent nest work card added

--- app/(publicGroup)/page.tsx
+++ app/(publicGroup)/page.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Hero from "./_components/Hero";
 import FeatureProperties from "./_components/FeatureProperties";
+import Services from "./_components/Services";
 
 export default function Home() {
-    return (
-      <>
-        {/* Hero section */}
-        <Hero />
+  return (
+    <>
+      {/* Hero section */}
+      <Hero />
 
-        {/* Feature properties section */}
-        <FeatureProperties />
-      </>
-    );
-  }
+      {/* Feature properties section */}
+      <FeatureProperties />
+
+      {/* How RentNest works section */}
+      <Services />
+    </>
+  );
+}
